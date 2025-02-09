1b5944fbc6a5a839923229fda37b3b0d06c91cce
Shakil Ahmad <shakilahmad323@gmail.com>
2025-02-10 00:04:12 +0530

    added the password generation logic and toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,55 @@
 import { useState } from 'react'
 import './App.css'
+import { NC, SC, UC, LC } from './data/passChar';
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
+  const [numbers, setNumbers] = useState(false);
   const [upperCase, setUpperCase] = useState(false);
   const [lowerCase, setLowerCase] = useState(false);
-  const [numbers, setNumbers] = useState(false);
   const [symbols, setSymbols] = useState(false);
+  const [passLength, setPassLength] = useState(10);
+  const [resultPass, setResultPass] = useState('');
+
+  const createPassword = () => {
+    let charPass = '';
+    let finalPass = '';
+
+    if (numbers || upperCase || lowerCase || symbols) {
+      if (numbers) charPass += NC;
+      if (upperCase) charPass += UC;
+      if (lowerCase) charPass += LC;
+      if (symbols) charPass += SC;
+
+      for (let i = 0; i < passLength; i++) {
+        finalPass += charPass.charAt(Math.floor(Math.random() * charPass.length))
+      }
+
+      setResultPass(finalPass);
+    }
+    else {
+      alert("Please select any password type");
+    }
+  }
+
+  const copyPassword = () => {
+    navigator.clipboard.writeText(resultPass);
+    toast.info("password copied.");
+  }
+
 
   return (
     <>
       <div className="passwordBox">
         <h2>Password Generator</h2>
         <div className="passwordBoxIn">
-          <input type="text" />
-          <button className='cpyBtn'>COPY</button>
+          <input type="text" defaultValue={resultPass} />
+          <button className='cpyBtn' onClick={copyPassword}>COPY</button>
         </div>
 
         <div className="passwordLength">
           <label>Password length</label>
-          <input type="number" max={20} />
+          <input type="number" value={passLength} min={10} onChange={(event) => setPassLength(event.target.value)} max={20} />
         </div>
 
         <div className="includeNumber checkBoxSection">
@@ -41,9 +72,10 @@ function App() {
           <input type="checkbox" checked={symbols} onChange={() => setSymbols(!symbols)} />
         </div>
 
-        <button className='btn'>Generate Password</button>
+        <button className='btn' onClick={createPassword}>Generate Password</button>
 
       </div>
+      <ToastContainer />
     </>
   )
 }
